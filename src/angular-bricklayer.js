2ce9bfb5b7806c20966ad86f73d20272ed92da2c
Jonathan Hornung <jonathan.hornung@gmail.com>
2016-04-23 22:24:15 +0200

    added redraw functionality

--- src/angular-bricklayer.js
+++ src/angular-bricklayer.js
@@ -1,25 +1,33 @@
 angular.module('jtt_bricklayer', [])
-    .directive('bricklayer', function () {
+    .directive('bricklayer', ['$timeout', function ($timeout) {
         return {
             restrict: 'C',
             replace: 'false',
             link: function (scope, element, attrs) {
                 var bricklayer = new Bricklayer(element[0]);
 
+                $timeout(function () {
+                    bricklayer.reorderElements(bricklayer.columnCount);
+                }, 0);
+
                 scope.$on('bricklayer.append', function (event, element) {
-                    setTimeout(function () {
-                        bricklayer.append(element)
-                    }, 0)
+                    bricklayer.append(element);
+                    bricklayer.reorderElements(bricklayer.columnCount);
+                    $timeout(function () {
+                        bricklayer.reorderElements(bricklayer.columnCount);
+                    }, 0);
                 });
 
                 scope.$on('bricklayer.prepend', function (event, element) {
-                    setTimeout(function () {
-                        bricklayer.prepend(element)
-                    }, 0)
+                    bricklayer.prepend(element);
+                    bricklayer.reorderElements(bricklayer.columnCount);
+                    $timeout(function () {
+                        bricklayer.reorderElements(bricklayer.columnCount);
+                    }, 0);
                 });
             }
         }
-    })
+    }])
     .directive('bricklayerAppend', function () {
         return {
             require: '?bricklayer',
