6d3b3db9212648fc1e7dbe5adb217d2f40bd4df0
Jonathan Hornung <jonathan.hornung@gmail.com>
2016-04-23 22:25:10 +0200

    bump version to v1.0.1

--- dist/angular-bricklayer.js
+++ dist/angular-bricklayer.js
@@ -1,32 +1,40 @@
 /**
     @name: angular-bricklayer 
-    @version: 1.0.0 (23-04-2016) 
+    @version: 1.0.1 (23-04-2016) 
     @author:  
     @url: https://github.com/JohnnyTheTank/angular-bricklayer 
     @license: MIT
 */
 angular.module('jtt_bricklayer', [])
-    .directive('bricklayer', function () {
+    .directive('bricklayer', ['$timeout', function ($timeout) {
         return {
             restrict: 'C',
             replace: 'false',
             link: function (scope, element, attrs) {
                 var bricklayer = new Bricklayer(element[0]);
 
+                $timeout(function () {
+                    bricklayer.reorderElements(bricklayer.columnCount);
+                }, 0);
+
                 scope.$on('bricklayer.append', function (event, element) {
-                    setTimeout(function () {
-                        bricklayer.append(element)
-                    }, 0)
+                    bricklayer.append(element);
+                    bricklayer.reorderElements(bricklayer.columnCount);
+                    $timeout(function () {
+                        bricklayer.reorderElements(bricklayer.columnCount);
+                    }, 0);
                 });
 
                 scope.$on('bricklayer.prepend', function (event, element) {
-                    setTimeout(function () {
-                        bricklayer.prepend(element)
-                    }, 0)
+                    bricklayer.prepend(element);
+                    bricklayer.reorderElements(bricklayer.columnCount);
+                    $timeout(function () {
+                        bricklayer.reorderElements(bricklayer.columnCount);
+                    }, 0);
                 });
             }
         }
-    })
+    }])
     .directive('bricklayerAppend', function () {
         return {
             require: '?bricklayer',
